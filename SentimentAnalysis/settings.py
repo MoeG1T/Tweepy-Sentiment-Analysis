992f09459a564ef376c150de8f0bf2a674fa4662
hamada989 <hamada.issam2002@gmail.com>
2021-08-20 15:14:41 -0400

    settings

--- SentimentAnalysis/settings.py
+++ SentimentAnalysis/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'SentimentAnalysis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\lubna\Tweepy\main\templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
