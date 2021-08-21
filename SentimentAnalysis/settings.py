d3ad30b918a7f9cbed1b244344eb3d1c83a0a96b
hamada989 <hamada.issam2002@gmail.com>
2021-08-21 04:56:01 -0400

    settings

--- SentimentAnalysis/settings.py
+++ SentimentAnalysis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tweepy8.herokuapp.com']
+ALLOWED_HOSTS = ['tweepy8.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
